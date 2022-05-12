5bce4c6534adc6d5b41bf6e90734f45c2f00bf60
PAMELLA CHRISTINI PERETO E SILVA <pamella.cpesilva@senacsp.edu.br>
2022-05-12 17:28:09 -0300

    Estudo React

--- terceiro/src/App.js
+++ terceiro/src/App.js
@@ -1,14 +1,51 @@
 import { useState } from "react";
+
 function App() {
-  const [numero, setNumero] = useState(1);
+  const [produto, setProduto] = useState([
+    {
+      id: 500,
+      nome: "Teclado",
+      categoria: "Informática",
+      preco: 50.5,
+    },
+    {
+      id: 501,
+      nome: "Mouse",
+      categoria: "Informática",
+      preco: 150.9,
+    },
+  ]);
   return (
     <div>
-      <p>O número atual é {numero}</p>
-      <p>
-        <button title="Clique" onClick={() => setNumero(numero + 1)}>
-          Clique aqui, colega!
-        </button>
-      </p>
+      <button
+        onClick={() => {
+          let id = prompt("Entre com o id do produto");
+          let nome = prompt("Entre com o nome do produto");
+          let categoria = prompt("Entre com a categoria do produto");
+          let preco = prompt("Entre com o preço do produto");
+          setProduto(
+            produto.concat({
+              id: id,
+              nome: nome,
+              categoria: categoria,
+              preco: preco,
+            })
+          );
+        }}
+      >
+        Adicionar novo produto
+      </button>
+      <h2>Produtos Cadastrados</h2>
+      {produto.map((item, index) => (
+        <div key={index}>
+          <h3>{item.nome}</h3>
+          <ul>
+            <li>Código do produto: {item.id}</li>
+            <li>Categoria do produto: {item.categoria}</li>
+            <li>Preço do produto: {item.preco}</li>
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
