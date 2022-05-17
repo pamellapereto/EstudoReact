1ce9c56236ea1ed1609243b1a6fe46807cdae8ca
pamellapereto <88159551+pamellapereto@users.noreply.github.com>
2022-05-17 16:28:30 -0300

    Update App.js

--- primeiro/src/App.js
+++ primeiro/src/App.js
@@ -6,7 +6,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Meu Primeiro Projeto em REACT</p>
+        <p>Nesse projeto quem manda é a Pamella Pereto.</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
